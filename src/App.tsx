4ccef3816163619bc250bb88f347341795216a2f
marian <anzinine@gmail.com>
2025-11-13 00:45:38 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import Landing from './components/landing/landing'
+
+export default function App() {
+  return <Landing />
+}
---
+++ src/components/landing/landing.tsx
@@ -0,0 +1,30 @@
+import './Landing.css'
+
+export default function Landing() {
+  return (
+    <main className="page">
+      <div className="container"></div>
+
+      
+      <div className="card">
+        <div className="title-row">
+          <div className="title-greenfield">Greenfield</div>
+          <div className="title">Capital Markets</div>
+        </div>
+      </div>
+
+      
+      <div className="card">
+        <h1>VIDEO</h1>
+      </div>
+
+      <div className="card">
+        <div className="title-greenfield"> Coming Soon</div>
+        <p className="desc">
+          Estamos preparando algo bueno. Volvé pronto.
+        </p>
+      </div>
+      
+    </main>
+  )
+}
